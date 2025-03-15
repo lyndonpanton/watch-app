dd2ed10eb040cc5de2816757cfeabe9ce1cf303a
Lyndon Panton <LyndonPanton@gmail.com>
2025-03-15 21:10:17 +0000

    Sidebar can be toggled into and out of view

--- web/core/js/script.js
+++ web/core/js/script.js
@@ -1,9 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     setYear();
+
+    let hamburger = document.getElementById("hamburger");
+    hamburger.addEventListener("click", toggleSidebar);
     
     function setYear() {
         let currentYear = new Date().getFullYear();
         let currentYearElement = document.getElementById("current-year");
         currentYearElement.textContent = currentYear;
+    }
+
+    function toggleSidebar(event) {
+        let sidebar = document.getElementById("watch-information");
+
+        if (sidebar.classList.contains("sidebar-open")) {
+            sidebar.classList.remove("sidebar-open");
+        } else {
+            sidebar.classList.add("sidebar-open");
+        }
     }
 });
