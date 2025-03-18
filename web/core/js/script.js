4dd957db48d4e77d3150f6597c75ee960af554ed
Lyndon Panton <LyndonPanton@gmail.com>
2025-03-18 16:40:41 +0000

    Clock shows the current time when page is loaded

--- web/core/js/script.js
+++ web/core/js/script.js
@@ -1,8 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     setYear();
+    setWatchClock();
 
     let hamburger = document.getElementById("hamburger");
     hamburger.addEventListener("click", toggleSidebar);
+
+    function setWatchClock() {
+        let hourElement = document.getElementById("watch-clock-digital-hour");
+        let minuteElement = document.getElementById("watch-clock-digital-minute");
+        let secondElement = document.getElementById("watch-clock-digital-second");
+
+        let currentDate = new Date();
+
+        hourElement.textContent = currentDate.getHours();
+        minuteElement.textContent = currentDate.getMinutes();
+        secondElement.textContent = currentDate.getSeconds();
+
+        // setInterval to start the clock…
+    }
     
     function setYear() {
         let currentYear = new Date().getFullYear();
